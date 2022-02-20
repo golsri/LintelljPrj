8fef2de13188fddf6c70a91578ce644c9e40dccc
gsrih <100052223+gsrih@users.noreply.github.com>
2022-02-19 19:04:07 -0500

    Added plus symbol in the test class
    Added plus symbol in the test class

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -1,7 +1,7 @@
 public class DevTestClass {
     public static void main(String[] args) {
         System.out.println("First Java Class");
-        System.out.println("DevTestClass");
+        System.out.println("DevTest+++Class");
         System.out.println("This is 3rd print for Github lesson ");
     }
 }
