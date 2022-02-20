ca06fc470666239c820b473b0ac2369a4d385a0d
gsrih <100052223+gsrih@users.noreply.github.com>
2022-02-19 19:31:16 -0500

    Update DevTestClass with 4th print (#2)
    Update DevTestClass with 4th print

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -3,5 +3,6 @@ public class DevTestClass {
         System.out.println("First Java Class");
         System.out.println("DevTest---Class");
         System.out.println("This is 3rd print for Github lesson ");
+        System.out.println("This is 4th print for Github lesson ");
     }
 }
